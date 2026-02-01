5a2496c2f96542a78d6bf839c49e0e270cdc4e01
David B <dbiagas@gmail.com>
2026-02-01 16:00:21 -0600

    fix css rendering

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{astro,js,jsx,ts,tsx}",
-    "./components/**/*.{astro,js,jsx,ts,tsx}",
-    "./views/**/*.{astro,js,jsx,ts,tsx}",
-    "./lib/**/*.{astro,js,jsx,ts,tsx}",
-    "./services/**/*.{astro,js,jsx,ts,tsx}",
-    "./App.tsx",
-    "./index.tsx"
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./lib/**/*.{js,jsx,ts,tsx}",
+    "./services/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {}
