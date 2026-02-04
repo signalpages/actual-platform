649b119384df565e6c6afb7b7135cfc1e9f2aa6a
David B <dbiagas@gmail.com>
2026-02-03 20:59:49 -0600

    fix(build): add 'use client' to ProductDetailView to support hooks

--- components/ProductDetailView.tsx
+++ components/ProductDetailView.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useRouter, useSearchParams } from 'next/navigation';
 import Link from 'next/link';
